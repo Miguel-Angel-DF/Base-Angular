db562dd435496339810a37d3ecca6667e0ef15c5
Miguell <mangeldf.42@gmail.com>
2022-02-09 14:19:53 -0600

    Segundo respaldo

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor( private http: HttpClient) { }
+
+  getPosts(){//metodo
+    return this.http.get( 'http://jsonplaceholder.typicode.com/posts')
+    .pipe ( tap(console.log));
+      }
+}
+ 
